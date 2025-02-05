9f07367ff25057c37591092fd2ae26f143897698
Jesús Raigón <150148012+Jarmuben@users.noreply.github.com>
2025-02-05 13:24:56 +0100

    Añadido caso práctico 1
    He añadido el caso practico 1 con todos los archivos de código, enunciado del ejercicio y explicaciones de como se ha realizado.

---
+++ Desarrollo_Web_Entorno_Cliente/caso_practico1/caso_practico_ud1.js
@@ -0,0 +1,6 @@
+alert(
+  "Pulsa en el botón Aceptar si deseas conocer que es HTML, CSS y JavaScript"
+);
+
+/*Este es el documento de JavaScript que he creado para darle interactividad a la página de tal modo que sale
+una alerta con un mensaje donde invita al usuario a pulsar el botón de Aceptar para continuar y conocer más.*/
